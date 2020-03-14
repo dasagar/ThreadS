722cf01d43e6a8db75c5cd16ef4f0a34eabed969
Dayawan Sagar <dayawansagar@gmail.com>
2020-03-14 13:46:07 +0530

    Thread Types

---
+++ ThreadTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadTypes
+{
+    class Program
+    {
+        // C# program to illustrate the 
+        // concept of foreground thread 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Thread thread = new Thread(ForeGroundThread);
+            thread.Start();
+            Console.WriteLine("Main Thread Ends");
+            Console.ReadLine();
+        }
+
+        // Static method 
+        static void ForeGroundThread()
+        {
+            for (int c = 0; c <= 3; c++)
+            {
+
+                Console.WriteLine("ForeGroundThread is in progress!!");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("ForeGroundThread ends!!");
+        }
+    }
+}
